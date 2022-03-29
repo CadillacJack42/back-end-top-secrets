2689466cc0500eedac6436fe29ce00e12a93ae0e
James Keicher <jamesdkeicher@gmail.com>
2022-03-29 12:17:38 -0700

    Add new table for user data. Write function in test to handle user creation in tests. Test unfinished

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,29 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const testUser = {
+  first_name: 'First',
+  last_name: 'Last',
+  email: 'me@mine.com',
+  password: '123456',
+};
+
+const createNewUser = async (userData = {}) => {
+  const password = userData.password ?? testUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({
+    ...testUser,
+    ...userData,
+  });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+
+  return [agent, user];
+};
+
 describe('backend-top-secret routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +33,15 @@ describe('backend-top-secret routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('Should `sign up` a user and enter their data into user table', async () => {
+    const expected = {
+      id: expect.any(String),
+      first_name: 'First',
+      last_name: 'Last',
+      email: 'me@mine.com',
+      user_id: expect.any(String),
+    };
   });
 });
